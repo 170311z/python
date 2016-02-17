7f9212b61376203086fe788e20d19b1779b58812
170311z <ifsec111000@gmail.com>
2016-02-17 18:09:20 +0900

    TCPとのパラメータの受け渡しを追記

--- TCPclient.py
+++ TCPclient.py
@@ -2,8 +2,8 @@
 
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "192.168.33.10"
+target_port = 9999
 
 #ソケットオブジェクトの作成
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -12,11 +12,12 @@ client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((target_host,target_port))
 
 #データの送信
-client.send("GET / HTTP/1.1\r\nHost : google.com\r\n\r\n")
+#client.send("GET / HTTP/1.1\r\nHost : google.com\r\n\r\n")
+client.send("ABCDEF")
 
 #データの受信
-response = client.recv(4096)
+#response = client.recv(4096)
 
-print response
+#print response
 
 
